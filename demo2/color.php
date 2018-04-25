a4936dea6515b47dbb6acda19ed76e8ee2abe0c7
17734027950 <17734027950@qq.com>
2018-04-25 14:50:57 +0800

    '1.0.2'

---
+++ demo2/color.php
@@ -0,0 +1,32 @@
+<?php
+// error_reporting(0);
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use League\ColorExtractor\Color;
+use League\ColorExtractor\ColorExtractor;
+use League\ColorExtractor\Palette;
+
+$palette = Palette::fromFilename('./img/1.jpg');
+
+// $palette is an iterator on colors sorted by pixel count
+foreach($palette as $color => $count) {
+    // colors are represented by integers
+    echo Color::fromIntToHex($color), ': ', $count, "\n";
+}
+
+// it offers some helpers too
+$topFive = $palette->getMostUsedColors(5);
+
+$colorCount = count($palette);
+
+$blackCount = $palette->getColorCount(Color::fromHexToInt('#000000'));
+
+
+// an extractor is built from a palette
+$extractor = new ColorExtractor($palette);
+
+// it defines an extract method which return the most “representative” colors
+$colors = $extractor->extract(5);
+
+?>
